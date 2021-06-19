afdcc882dca4c42499b75be4c180b6341a06f23b
aleks <aleks@DESKTOP-B0VKUMD>
2021-06-20 01:49:01 +0600

    Добавьте файлы проекта.

---
+++ homeWork1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace homeWork1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int age;
+            uint damage;
+            float time;
+            double coins;
+            long miles;
+            byte daysCount;
+            sbyte temperature;
+            string name;
+            char buttonSign;
+            bool isItAlive;
+        }
+    }
+}
